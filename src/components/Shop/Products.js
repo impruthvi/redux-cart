cdcbcd5a66dac137bd6a68dc89ce432afd7952c3
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-08-29 18:27:41 +0530

    created a redux for add to cart

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,46 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    title: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    title: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    title: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    title: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_MEALS.map((product) => (
+          <ProductItem
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
